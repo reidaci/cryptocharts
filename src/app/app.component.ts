76c1ca8f154ebb2d79c2dd17e3ed6d300db89fec
Rei Daci <reiz.daci@outlook.com>
2024-02-07 11:38:51 +0100

    crypto prices chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { Chart } from 'chart.js/auto';
+import { ChartService } from './services/chart.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'charts';
+
+chart: any = []
+result: any
+coinPrice: any;
+coinName:any
+
+
+constructor(private service: ChartService){}
+
+ngOnInit() {
+  this.service.cryptoData().subscribe((res) => {
+    this.result = res;
+    this.coinPrice = this.result.data.coins.map((coins: any) => coins.price);
+    this.coinName = this.result.data.coins.map((coins: any) => coins.name);
+    console.log(this.coinPrice);
+    console.log(this.coinName);
+
+    this.chart = new Chart('canvas', {
+      type: 'pie',
+      data: {
+        labels: this.coinName,
+        datasets: [
+          {
+            data: this.coinPrice,
+            borderColor: 'black',
+            label: 'Coin Price',
+            backgroundColor:  ['#CB4335', '#1F618D', '#F1C40F', '#27AE60', '#884EA0', '#D35400'],
+            borderWidth: 3,
+          },
+        ],
+      },
+    });
+  });
+}
 }
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  API_KEY: 'coinranking8dd14f20c40879f57b2e5b38c43f237ecd4b0ed9fd8104d6'
+}
